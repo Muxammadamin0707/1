d6a38095c84c07c4de738d5a39d911b492cd57b4
Muxammadamin Zoirov <muxammadaminzoirov@gmail.com>
2024-06-07 12:04:04 +0500

    version 2

--- index.js
+++ index.js
@@ -1,16 +1,42 @@
 let input =  document.querySelector("input");
 let button =  document.querySelector("button");
-let ul =  document.querySelector("ul");
+let list =  document.querySelector("ul");
 
 button.addEventListener("click", (event) =>{
     event.preventDefault();
 
-    let vaziva = input.value;
+    let vazifa = input.value;
     let li = document.createElement("li");
+    let span = document.createElement("span");
+    let done = document.createElement("button");
+    let del = document.createElement("button")
 
-    li.textContent = vaziva;
 
-    ul.appendChild(li);
+    done.addEventListener("click", (event) => {
+        event.preventDefault();
+
+        span.classList.toggle("line");
+    });
+
+    del.addEventListener("click", (event) => {
+        event.preventDefault();
+
+
+        li.remove();
+    } )
+
+    done.classList.add("doneBtn");
+    del.classList.add("donel");
+
+    span.textContent = vazifa;
+    done.textContent = "done";
+    del.textContent = "Delete";
+
+    li.appendChild(span);
+    li.appendChild(done);
+    li.appendChild(del);
+
+    list.appendChild(li);
 
     input.value = "";
 });
